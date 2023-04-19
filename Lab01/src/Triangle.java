63067d935c210c41e861499716491e10183f305e
Khanh Cao <caokhanh03.ck@gmail.com>
2023-04-19 21:42:23 +0700

    Update Triangle.java

--- Lab01/src/Triangle.java
+++ Lab01/src/Triangle.java
@@ -1,9 +1,11 @@
+import java.util.*;
+
 public class Triangle {
     public static void main(String[] args) {
         int n;
-        Scanner keyboard = new Scanner(System.in);
-        n = keyboard.nextInt();
-        scan.close();
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+        input.close();
         System.out.println(n);
         for (int i=0; i<n; i++) {
             for (int j=n-i; j>0; j--) {
