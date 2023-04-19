576dc96752e1e87199252492bef7330fbca814ee
Khanh Cao <87952980+jakhin03@users.noreply.github.com>
2023-04-19 10:33:35 +0700

    Update Triangle.java

--- Lab01/Triangle.java
+++ Lab01/Triangle.java
@@ -1,19 +1,21 @@
- class Triangle{
+public class Triangle {
     public static void main(String[] args) {
         int n;
         Scanner keyboard = new Scanner(System.in);
         n = keyboard.nextInt();
         scan.close();
         System.out.println(n);
-        
-        for (int i=0; i<n; i++){
-            for (int j=0; j<(n-i); j++){
+        for (int i=0; i<n; i++) {
+            for (int j=n-i; j>0; j--) {
                 System.out.print(" ");
             }
-            System.out.print("*");
-            for (int j=0; j<(n-1); j++){
+            for (int j=i*2+1; j>0; j--) {
+                System.out.print("*");
+            }
+            for (int j=n-i; j>0; j--) {
                 System.out.print(" ");
             }
+            System.out.println();
         }
     }
 }
